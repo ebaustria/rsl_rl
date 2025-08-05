1caeec2447b50624197d1e9070b2cab5f56a479c
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2025-08-05 14:12:06 +0200

    Adapt ActorCritic and ActorCriticRecurrent to jax

--- rsl_rl/modules/actor_critic_recurrent.py
+++ rsl_rl/modules/actor_critic_recurrent.py
@@ -28,30 +28,32 @@
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
-import numpy as np
+import jax
+import jax.numpy as jnp
+from flax import nnx
+from flax.nnx import RNNCellBase
 
-import torch
-import torch.nn as nn
-from torch.distributions import Normal
-from torch.nn.modules import rnn
-from .actor_critic import ActorCritic, get_activation
 from rsl_rl.utils import unpad_trajectories
+from .actor_critic import ActorCritic, get_activation
+
 
 class ActorCriticRecurrent(ActorCritic):
     is_recurrent = True
-    def __init__(self,  num_actor_obs,
-                        num_critic_obs,
-                        num_actions,
-                        actor_hidden_dims=[256, 256, 256],
-                        critic_hidden_dims=[256, 256, 256],
-                        activation='elu',
-                        rnn_type='lstm',
-                        rnn_hidden_size=256,
-                        rnn_num_layers=1,
-                        init_noise_std=1.0,
-                        **kwargs):
+
+    def __init__(self, num_actor_obs,
+                 num_critic_obs,
+                 num_actions,
+                 actor_hidden_dims=[256, 256, 256],
+                 critic_hidden_dims=[256, 256, 256],
+                 activation='elu',
+                 rnn_type='lstm',
+                 rnn_hidden_size=256,
+                 rnn_num_layers=1,
+                 init_noise_std=1.0,
+                 **kwargs):
         if kwargs:
-            print("ActorCriticRecurrent.__init__ got unexpected arguments, which will be ignored: " + str(kwargs.keys()),)
+            print("ActorCriticRecurrent.__init__ got unexpected arguments, which will be ignored: " + str(
+                kwargs.keys()), )
 
         super().__init__(num_actor_obs=rnn_hidden_size,
                          num_critic_obs=rnn_hidden_size,
@@ -63,8 +65,8 @@ class ActorCriticRecurrent(ActorCritic):
 
         activation = get_activation(activation)
 
-        self.memory_a = Memory(num_actor_obs, type=rnn_type, num_layers=rnn_num_layers, hidden_size=rnn_hidden_size)
-        self.memory_c = Memory(num_critic_obs, type=rnn_type, num_layers=rnn_num_layers, hidden_size=rnn_hidden_size)
+        self.memory_a = Memory(num_actor_obs, hidden_size=rnn_hidden_size, rnn_type=rnn_type)
+        self.memory_c = Memory(num_critic_obs, hidden_size=rnn_hidden_size, rnn_type=rnn_type)
 
         print(f"Actor RNN: {self.memory_a}")
         print(f"Critic RNN: {self.memory_c}")
@@ -73,9 +75,9 @@ class ActorCriticRecurrent(ActorCritic):
         self.memory_a.reset(dones)
         self.memory_c.reset(dones)
 
-    def act(self, observations, masks=None, hidden_states=None):
+    def act(self, observations: jax.Array, masks=None, hidden_states=None):
         input_a = self.memory_a(observations, masks, hidden_states)
-        return super().act(input_a.squeeze(0))
+        return super().act(jnp.squeeze(input_a, axis=0))
 
     def act_inference(self, observations):
         input_a = self.memory_a(observations)
@@ -84,30 +86,27 @@ class ActorCriticRecurrent(ActorCritic):
     def evaluate(self, critic_observations, masks=None, hidden_states=None):
         input_c = self.memory_c(critic_observations, masks, hidden_states)
         return super().evaluate(input_c.squeeze(0))
-    
+
     def get_hidden_states(self):
         return self.memory_a.hidden_states, self.memory_c.hidden_states
 
 
-class Memory(torch.nn.Module):
-    def __init__(self, input_size, type='lstm', num_layers=1, hidden_size=256):
-        super().__init__()
-        # RNN
-        rnn_cls = nn.GRU if type.lower() == 'gru' else nn.LSTM
-        self.rnn = rnn_cls(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers)
+class Memory(nnx.Module):
+    def __init__(self, input_size: int, hidden_size: int = 256, rnn_type: str = "lstm"):
+        self.rnn: RNNCellBase = nnx.LSTMCell(in_features=input_size,
+                                             hidden_features=hidden_size) if rnn_type.lower() == "lstm" else nnx.GRUCell(
+            in_features=input_size, hidden_features=hidden_size)
         self.hidden_states = None
-    
-    def forward(self, input, masks=None, hidden_states=None):
+
+    def __call__(self, x: jax.Array, masks=None, hidden_states=None) -> jax.Array:
         batch_mode = masks is not None
         if batch_mode:
-            # batch mode (policy update): need saved hidden states
             if hidden_states is None:
                 raise ValueError("Hidden states not passed to memory module during policy update")
-            out, _ = self.rnn(input, hidden_states)
+            carry, out = self.rnn(hidden_states, x)
             out = unpad_trajectories(out, masks)
         else:
-            # inference mode (collection): use hidden states of last step
-            out, self.hidden_states = self.rnn(input.unsqueeze(0), self.hidden_states)
+            self.hidden_states, out = self.rnn(self.hidden_states, jnp.expand_dims(x, axis=0))
         return out
 
     def reset(self, dones=None):
